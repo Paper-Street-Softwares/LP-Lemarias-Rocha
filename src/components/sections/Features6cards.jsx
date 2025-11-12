38bc6d6b9600ba48e234f4cf9342e3fa797cc002
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-12 08:30:26 -0300

    fix: Adiciona a cir preta e sombra no título da Feature

--- src/components/sections/Features6cards.jsx
+++ src/components/sections/Features6cards.jsx
@@ -41,7 +41,7 @@ export default function Features6cards({ colorMode }) {
         miniTitle={t("features.miniTag")}
         sectionHeaderTitle={
           <span
-            className="tracking-[5px]"
+            className="tracking-[5px] text-black [text-shadow:_0_2px_4px_rgb(0_0_0_/_40%)]"
             dangerouslySetInnerHTML={{ __html: t("features.title") }}
           />
         }
